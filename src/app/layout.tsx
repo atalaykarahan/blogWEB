79b72e67c79615ac6ef0b1dd1cd25c6f5ff31e5a
atalaykarahan <krhnatalay@gmail.com>
2024-09-19 21:18:27 +0300

    favicon kısmı ayarlandı ve tamamlandı

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,65 +1,66 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {CMS_NAME, HOME_OG_IMAGE_URL} from "@/lib/constants";
+import type {Metadata} from "next";
+// import { Inter } from "next/font/google";
 import cn from "classnames";
-import { ThemeSwitcher } from "./_components/theme-switcher";
+import {ThemeSwitcher} from "./_components/theme-switcher";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Atalay Karahan`,
+    title: `Atalay Karahan`,
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta
-          name="msapplication-config"
-          content="/favicon/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#000" />
-        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      </head>
-      <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
-      >
-        <ThemeSwitcher />
+    return (
+        <html lang="en">
+        <head>
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/favicon/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
+            <link
+                rel="mask-icon"
+                href="/favicon/safari-pinned-tab.svg"
+                color="#000000"
+            />
+            <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+            <meta name="msapplication-TileColor" content="#000000"/>
+            <meta
+                name="msapplication-config"
+                content="/favicon/browserconfig.xml"
+            />
+            <meta name="theme-color" content="#000"/>
+            <link rel="alternate" type="application/rss+xml" href="/feed.xml"/>
+        </head>
+        <body
+            // className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
+            className={cn('font-inter', "dark:bg-slate-900 dark:text-slate-400")}
+        >
+        <ThemeSwitcher/>
         <div className="min-h-screen">{children}</div>
-        <Footer />
-      </body>
-    </html>
-  );
+        <Footer/>
+        </body>
+        </html>
+    );
 }
